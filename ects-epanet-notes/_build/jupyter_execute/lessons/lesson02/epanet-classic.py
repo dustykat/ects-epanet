a1ba12d0bcc8d25efad04e88ef7e7be04b0ecb93
theodore cleveland <ted.cleveland@gmail.com>
2023-06-08 21:36:31 +0000

    include file access notes

--- ects-epanet-notes/_build/jupyter_execute/lessons/lesson02/epanet-classic.py
+++ ects-epanet-notes/_build/jupyter_execute/lessons/lesson02/epanet-classic.py
@@ -26,7 +26,9 @@
 # 
 # An on-line version is hosted at [http://freeswmm.ddns.net](http://freeswmm.ddns.net) described herein uses the WINE application layer to run an implementation of EPANET with the "Classic" GUI on a Linux server, on an Intel x86-64 chipset.  
 # 
-# Access to the online desktop is as simple as 
+# :::{Note}
+# The on-line version is really for the author of this book to demonstrate EPANET but also allow workshop participants to also use the environment.  End users are far better off using their own hardware, but the shared space is a reasonable training option - host to workstation file transfer is via a JupyterHub instance on the FreeSWMM host (user is **antares**; password is **freeswmm**) which I will demonstrate in-class.
+# :::
 # 
 # ## Accessing the FreeSWMM host
 # 
@@ -60,23 +62,62 @@
 # 
 # ![](xfcedesktop.png)
 # 
-# To start SWMM navigate to the WINE application and select SWMM 5.1.015 to launch the interface, as pictured below:
+# To start EPANET navigate to the WINE application and select EPANET2 to launch the interface, as pictured below:
 # 
 # ![](epanet2w.png)
 # 
-# When SWMM starts you should be presented with a clean interface as pictured below:
+# When EPANET starts you should be presented with a clean interface as pictured below:
 # 
 # ![](epanet2go.png)
 # 
 # :::{note}
 # EPANET is running in the WINE application layer on a linux server, it is similar to windows in terms of file management, resizing and moving the window showing EPANET is accomplished by right-click in the application tab in the upper part of the web interface; otherwise it is ordinary EPANET as one would experience on a Windows computer
 # :::
+
+# ## Running Existing Examples
+# 
+# The existing examples are all stored in the EPANET-Files Directory on the XFCE desktop (as well as some pre-installed examples).  As new examples are added they will be located in that directory.  
+# 
+# ![](epanet-ex1.png)
+# 
+# ## Moving files to/from the host
 # 
 # :::{warning}
-# The remote server is programmed to disconnect every 1 hour as a way to end abandoned or run-away processes.  Anything saved is preserved, but unsaved work is lost.  Keep an eye on the clock in the upper right corner of the desktop after connection to be aware of pending disconnect.  There is NO WARNING.
-# :::
-
+# Moving files to/from the remote server is non-trivial.  :::
 # 
+# File upload/download is by a JupyterHub instance running on the host.  On your computer access the JupyterHub server at [http://freeswmm.ddns.net:8000](http://freeswmm.ddns.net:8000) you should get a login window unless someone else is connected (or left a running instance) that looks like:
+# 
+# ![](antaresfreeswmm.png)
+# 
+# Enter the credentials from above and you will get a JupyterHub interface like below (it may take awhile to load)
+# 
+# ![](antaresjhub.png)
+# 
+# The EPANET files are located in `/Desktop/EPANET-Files` and to upload drag and drop from your computer to this directory (or a subdirectory).
+# 
+# To download, navigate to the target file in the JupyterHub, then select the file, right-click and download.
+# 
+# :::{note}
+# I will demonstrate in class, its easy to accomplish, but also easy to confuse yourself as you have two entirely different browser windows accessing the same location. After the demonstration it will be obvious.
+# :::
+# 
+# ## Exiting FreeSWMM
+# 
+# When you are done using the application, save your work then exit EPANET in the usual fashion. 
+# 
+# ![](endepanet.png)
+# 
+# Then select disconnect to close the web interface as pictured below:
+# 
+# ![](disconnect.png)
+# 
+# :::{warning}
+# Failure to exit EPANET before disconnecting will frequently stall the server.  It won't hurt anything, but is not end user fixable.
+# :::
+# 
+# :::{note}
+# The server is programmed to disconnect every hour and restart one minute later; if you are in the middle of something your work should be preserved, just wait the minute and reconnect.  This annoying behavior is to mitigate loss of CPU allocation on AWS because of an idle XFCE window being left open.   Please remember to disconnect your sessions, there are only about 2.4 hours/day of computation time available and the connection itself uses a fair portion of that allocation.
+# :::
 
 # ## EPANET by Example
 # 
