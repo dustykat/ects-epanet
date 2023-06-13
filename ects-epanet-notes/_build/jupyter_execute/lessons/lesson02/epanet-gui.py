c70c41d5ebeb11c0937c9c42e684321c511aacc9
theodore cleveland <ted.cleveland@gmail.com>
2023-06-13 22:10:41 +0000

    graphics ex1

--- ects-epanet-notes/_build/jupyter_execute/lessons/lesson02/epanet-gui.py
+++ ects-epanet-notes/_build/jupyter_execute/lessons/lesson02/epanet-gui.py
@@ -12,6 +12,120 @@
 # 
 
 # ### Example 1
+# 
+# A simple model to consider is a single pipe connecting two reservoirs.
+# 
+# A **problem statement** might be something like:
+# 
+# > A 5-foot diameter, enamel coated, steel pipe carries 60oF water at a dis-
+# charge of 295 cubic-feet per second (cfs). Using the Moody chart, estimate
+# the head loss in a 10,000 foot length of this pipe.
+# 
+# :::{note}
+# This example is verbatim from the references in the previous section, here the examples are presented using the freeswmm interface.  Be aware that the files are pre-placed, so one could simply open and go without building a model.
+# :::
+# 
+# In EPANET we will start the program, build a tank-pipe system and find the head
+# loss in a 10,000 foot length of the pipe. The program will compute the friction factor
+# for us (and we can check on the Moody chart if we wish).
+# 
+# The main trick in EPANET is going to be the friction coefficient, in the EPANET
+# manual on page 30 and 31, the author indicates that the program expects a roughness
+# coefficient based on the head loss equation. The units of the roughness coefficient for
+# a steel pipe are $0.15 \times 10^{-3}$ feet. On page 71 of the user manual the author states
+# that roughness coefficients are in millifeet (millimeters) when the Darcy-Weisbach
+# head loss model is used. So keeping that in mind we proceed with the example.
+# 
+# Figure xx is a screen capture of the EPANET program after connecting in freeswmm.
+# 
+# ![](./ex1/ex1ss1.png)
+# 
+# The program starts as a blank slate and we will select a reservoir and a node from
+# the tool bar at the top and place these onto the design canvas.
+# 
+# 
+# 
+# Figure 37 is a screen capture of the EPANET program after setting defaults for the
+# simulation. 
+# 
+# ![](./ex1/ex1ss2.png)
+# 
+# Failure to set correct units for your problem are sometimes hard to detect
+# (if the model runs), so best to make it a habit to set defaults for all new projects.
+# 
+# Next we add the reservoir and the node. Figure 41 is a screen capture after the reservoir and node are placed.  
+# 
+# ![](./ex1/ex1ss3.png)
+# 
+# :::{note}
+# The text tool was used to annotate the nodes - these are simply text blocks, and are not node labels, which is also a way to annotate.
+# :::
+# 
+# We will specify a total head at the reservoir (value is unimportant as long as it is big enough to overcome the head loss and not result in a
+# negative pressure at the node. We will specify the demand at the node equal to the
+# desired 
+# flow in the pipe. 
+# 
+# Next we will add the pipe.  Figure 43 is a screen capture after the pipe is placed. 
+# 
+# ![](./ex1/ex1ss4.png)
+# 
+# The sense of flow in this example is from reservoir to node, but if we had it backwards we could either accept a negative flow in the pipe, or right-click the pipe and reverse the start and end node connections.
+# 
+# Now we can go back to each hydraulic element in the model and edit the properties.
+# We supply pipe properties (diameter, length, roughness height) as in Figure 45. 
+# 
+# ![](./ex1/ex1ss5.png)
+# 
+# We supply the reservoir total head as in Figure 47.
+# 
+# ![](./ex1/ex1ss6.png)
+# 
+# We set the demand node elevation and the actual desired flow rate as in Figure 49. 
+# 
+# ![](./ex1/ex1ss7.png)
+# 
+# The program is now ready to run, next step would be to save the input file.
+# 
+# :::{note}
+# I will save an input file and a "network" file.  The network file can be run using a command-line version or using the programmers toolkit.  We will use this toolkit later on in the short course
+# :::
+# 
+# > Saving the network file <br>
+# > ![](./ex1/ex1ss8.png)
+# 
+# Now we can run the program by selecting the lightning bolt icon (or menu item "/Project/Run Analysis").
+# 
+# ![](./ex1/ex1ss9.png)
+# 
+# Upon completion
+# 
+# ![](./ex1/ex1ss10.png)
+# 
+# Yay! A sucessful run, which means that the nodal connectivity is OK and there are no computed negative pressures. A successful run means the program found an answer to the problem you provided - whether it is the correct answer to your problem requires the engineer to interpret results and decide if they make sense. 
+# 
+# The more common conceptualization errors are incorrect units and head loss equation for the supplied roughness values, missed connections, and forgetting demand somewhere.
+# With practice these kind of errors are straightforward to detect. In the present
+# example we select the pipe and the solution values are reported at the bottom of a dialog box.
+# 
+# Figure 54 is the result of turning on the computed head values at the node (and
+# reservoir) and the flow value for the pipe in the Map tab of the browser tool. 
+# 
+# ![](./ex1/ex1ss11.png)
+# 
+# The dialog box reports about 7.2 feet
+# of head loss per 1000 feet of pipe for a total of 72 feet of head loss in the system.
+# The total head at the demnad node is about 28 feet, so the head loss plus remaining
+# head at the node is equal to the 100 feet of head at the reservoir, the anticipated
+# result.
+# The computed friction factor is 0.010, which we could check against the Moody chart
+# if we wished to adjust the model to agree with some other known friction factor.
+# 
+# #### Files
+# 
+# 1. [EX1.bmp](http://freeswmm.ddns.net/ects-epanet/ects-epanet-notes/lessons/lesson02/ex1/EX1.bmp) The base image file
+# 2. [EX1.net](http://freeswmm.ddns.net/ects-epanet/ects-epanet-notes/lessons/lesson02/ex1/EX1.net) An EPANET input file (binary .net file, readable by the GUI)
+# 3. [EX1.net](http://freeswmm.ddns.net/ects-epanet/ects-epanet-notes/lessons/lesson02/ex1/EX1.net) A different EPANET input file (binary .net file, readable by the GUI)
 
 # ### Example 2
 
